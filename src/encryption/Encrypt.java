989e0eafcdc16faa5b9a37177552b35520dc804b
Johan Martinsson <martinsson.johan@gmail.com>
2012-12-04 02:02:08 +0100

    now the offset depends on system time for every line encoded

---
+++ src/encryption/Encrypt.java
@@ -0,0 +1,13 @@
+package encryption;
+
+
+public class Encrypt {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Enigma().encrypt();
+    }
+
+}
--- src/encryption/Enigma.java
+++ src/encryption/Enigma.java
@@ -3,11 +3,25 @@ package encryption;
 
 public class Enigma {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        new Leeter().encrypt();
+    public Enigma() {
     }
+    
+    public void encrypt() {
+        char[] line = System.console().readPassword();
+        char[] encryptedLine = shift(line);
+        System.console().printf("The encryption of %1$2s is : %2$2s\n", String.valueOf(line).replaceAll(".", "*"), String.valueOf(encryptedLine));
+    }
+
+    private char[] shift(char[] line) {
+        char[] encryptedLine = new char[line.length];
+        int wheelOffsetPosition = (int) (System.nanoTime() % 28);
+        for (int i = 0; i < line.length; i++) {
+            char c = line[i];
+            int j = c + wheelOffsetPosition;
+            encryptedLine[i] = (char)j;
+        }
+        return encryptedLine;
+    }
+
 
 }
