0edbaff7cad59a42f0fa40c94f9f840d65b9232f
Johan Martinsson <martinsson.johan@gmail.com>
2012-12-18 14:08:14 +0100

    add doc

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Your task is to test as much of the code as possible.
+ 
+In most cases, in order to make it testable you have to apply minimal refactorings. Use the IDE!
+
+You'll practice 
+ - add parameter to method (make static methods testable)
+ - Replace single
--- src/encryption/Enigma.java
+++ src/encryption/Enigma.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class Enigma {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
         encrypt();
     }
 
+    /**
+     * Test as much of the algorithm as possible
+     * Do minimal refactorings if needed
+     */
     private static void encrypt() {
         String line = new Scanner(System.in).nextLine();
         System.out.println("You entered " + line.replaceAll("e", "3") + "\n");
