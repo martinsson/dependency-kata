7f04184e4208a3f6094842387204c725f555d5aa
Johan Martinsson <martinsson.johan@gmail.com>
2012-12-18 13:06:51 +0100

    converted to maven

---
+++ test/encryption/EnigmaTest.java
@@ -0,0 +1,14 @@
+package encryption;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import java.io.Console;
+
+import org.junit.Test;
+
+public class EnigmaTest {
+
+    
+
+}
