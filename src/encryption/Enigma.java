c2814358f6ef9f05fbec2951edefbce931935bf9
Johan Martinsson <martinsson.johan@gmail.com>
2013-01-16 00:07:24 +0100

    injection of singletons into instance

--- src/encryption/Enigma.java
+++ src/encryption/Enigma.java
@@ -3,19 +3,11 @@ package encryption;
 import java.util.Scanner;
 
 
+
 public class Enigma {
 
     public static void main(String[] args) {
-        encrypt();
-    }
-
-    /**
-     * Test as much of the algorithm as possible
-     * Do minimal refactorings if needed
-     */
-    private static void encrypt() {
         String line = new Scanner(System.in).nextLine();
-        System.out.println("You entered " + line.replaceAll("e", "3") + "\n");
+        new Leeter().encrypt(line);
     }
-
 }
---
+++ src/encryption/Leeter.java
@@ -0,0 +1,19 @@
+package encryption;
+
+
+public class Leeter {
+
+    
+    public Leeter() {
+    }
+    
+    /**
+     * Test as much of the algorithm as possible
+     * Do minimal refactorings if needed
+     * @param line TODO
+     */
+    public void encrypt(String line) {
+        System.out.println("You entered " + line.replaceAll("e", "3") + "\n");
+    }
+    
+}
